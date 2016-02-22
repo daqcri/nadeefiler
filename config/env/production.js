b18122791f9db0b20bf13e20e547e809376c7dea
Hossam Hammady <github@hammady.net>
2016-02-22 22:07:23 +0300

    Creating projects api resource in sails

--- config/env/production.js
+++ config/env/production.js
@@ -17,9 +17,10 @@ module.exports = {
    * environment (see config/connections.js and config/models.js )           *
    ***************************************************************************/
 
-  // models: {
-  //   connection: 'someMysqlServer'
-  // },
+  models: {
+    connection: 'localPostgres',
+    migrate: 'safe'
+  },
 
   /***************************************************************************
    * Set the port in the production environment to 80                        *
@@ -35,4 +36,7 @@ module.exports = {
   //   level: "silent"
   // }
 
+  blueprints: {
+    shortcuts: false
+  }
 };
